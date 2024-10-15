6705a647836828a0030f5822d04e3bd1460b4cb2
Yiming0576 <Yiming0576@gmail.com>
2024-10-15 09:41:38 -0400

    add main application entry class and utils folder

---
+++ src/main/java/com/pluralsight/AccountingLedgerAPP.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+
+public class AccountingLedgerAPP {
+
+    public static void main(String[] args) {
+        homeScreen();
+    }
+
+    private static void homeScreen() {
+    }
+}
